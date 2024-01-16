cd8f6d6980522c3db50f0d62a4bfe63caac920c9
Andika Alakate <andikaalakate@gmail.com>
2024-01-16 15:30:15 +0700

    Init TailwindCSS

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
+
+Route::get('/welcome', function () {
+    return view('welcome');
 });
