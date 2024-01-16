8118ee5af9e7e7cc3827a415e863376d009b3553
Andika Alakate <andikaalakate@gmail.com>
2024-01-16 16:30:42 +0700

    Bangsat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,89 +1,38 @@
-<!doctype html>
-<html lang="en">
-
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="theme-color" content="#000000">
     <title>Absensi SMKS Jambi Medan - Software Engineering</title>
-    <meta name="description" content="Mobilekit HTML Mobile UI Kit">
-    <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" sizes="32x32">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/icon/192x192.png') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="manifest" href="{{ asset('__manifest.json') }}">
+    <link rel="stylesheet" href="{{ asset('login.scss') }}">
 </head>
-
-<body class="bg-white max-w-4xl">
-
-    <!-- loader -->
-    <div id="loader">
-        <div class="spinner-border text-primary" role="status"></div>
-    </div>
-    <!-- * loader -->
-
-
-    <!-- App Capsule -->
-    <div id="appCapsule" class="pt-0">
-
-        <div class="login-form mt-1">
-            <div class="section">
-                <img src="{{ asset('assets/img/sample/photo/vector4.png') }}" alt="image" class="form-image">
-            </div>
-            <div class="section mt-1">
-                <h1>Get started</h1>
-                <h4>Fill the form to log in</h4>
-            </div>
-            <div class="section mt-1 mb-5">
-                <form action="/proseslogin" method="POST">
-                    @csrf
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <input type="text" name="nis" class="form-control" id="nis" placeholder="NIS">
-                            <i class="clear-input">
-                                <ion-icon name="close-circle"></ion-icon>
-                            </i>
-                        </div>
-                    </div>
-
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Password">
-                            <i class="clear-input">
-                                <ion-icon name="close-circle"></ion-icon>
-                            </i>
-                        </div>
-                    </div>
-
-                    <div class="form-button-group">
-                        <button type="submit" class="btn btn-primary btn-block btn-lg">Log in</button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-
-
-    </div>
-    <!-- * App Capsule -->
-
-        <!-- ///////////// Js Files ////////////////////  -->
-    <!-- Jquery -->
-    <script src="{{ asset('assets/js/lib/jquery-3.4.1.min.js') }}"></script>
-    <!-- Bootstrap-->
-    <script src="{{ asset('assets/js/lib/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/lib/popper.min.js') }}"></script>
-    <!-- Ionicons -->
-    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
-    <!-- Owl Carousel -->
-    <script src="{{ asset('assets/js/plugins/owl-carousel/owl.carousel.min.js') }}"></script>
-    <!-- jQuery Circle Progress -->
-    <script src="{{ asset('assets/js/plugins/circle-progress/circle-progress.min.js') }}"></script>
-    <!-- Base Js File -->
-    <script src="{{ asset('assets/js/base.js') }}"></script>
+<body>
+<div class="container">
+  <section id="content">
+    <form action="/proseslogin" method="POST">
+        @csrf
+      <h1>Login Form</h1>
+      <div>
+        <input type="text" placeholder="NIS" name="nis" required="" id="username" />
+      </div>
+      <div>
+        <input type="password" placeholder="Password" name="password" required="" id="password" />
+      </div>
+      <div>
+        <input type="submit" value="Log in" />
+        <a href="#">Lost your password?</a>
+        <a href="#">Register</a>
+      </div>
+    </form><!-- form -->
+    
+  </section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
 </body>
 
 </html>
