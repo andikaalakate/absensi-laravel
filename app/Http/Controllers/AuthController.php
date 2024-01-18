2da37ca6a26a908a14dc136f896067b1c9fa480d
Andika Alakate <andikaalakate@gmail.com>
2024-01-18 18:20:29 +0700

    Login Page Selesai~

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,16 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function home()
+    {
+        return redirect('/login');
+    }
+    public function index()
+    {
+        return view('auth.login');
+    }
     public function proseslogin(Request $request)
     {
         if (Auth::guard('siswa')->attempt(['nis' => $request->nis, 'password' => $request->password])) {
-            return redirect('/welcome');
+            return redirect('/siswa/dashboard');
         } else {
-            echo "Login Gagal";
+            return redirect('/login')->with('warning', 'NIS / Password Salah!');
+        }
+    }
+    public function logout()
+    {
+        if (Auth::guard('siswa')->check()) {
+            Auth::guard('siswa')->logout();
+            return redirect('/login');
         }
     }
 }
