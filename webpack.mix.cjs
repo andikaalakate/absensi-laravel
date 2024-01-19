a804bc0c33c9b07a2783498c7910174db7a7edbd
Andika Alakate <andikaalakate@gmail.com>
2024-01-19 11:14:07 +0700

    Rombak Lagi

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,9 +1,24 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
 
-mix.postCss(
+mix
+.postCss(
     "resources/css/app.css", // File masukan (input file)
     "public/css", // Folder keluaran (output folder)
     [tailwindcss("tailwind.config.js")] // Plugin Tailwind CSS
-).css("resources/css/style.css", "public/css");
+)
+.postCss(
+    "resources/css/tailwind.css", // File masukan (input file)
+    "public/css", // Folder keluaran (output folder)
+    [tailwindcss("tailwind.config.js")] // Plugin Tailwind CSS
+)
+.css(
+    "resources/assets/login/css/style.css",
+    "public/assets/login/css"
+)
+.js(
+    "resources/assets/login/js/main.js",
+    "public/assets/login/js"
+)
+;
 
